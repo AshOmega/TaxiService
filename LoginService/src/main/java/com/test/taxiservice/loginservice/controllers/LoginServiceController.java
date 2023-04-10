6a1bdb9518e5b1799be698d07bed0f535096154c
karollil <karollil@adobe.com>
2023-04-10 19:59:33 +0530

    JWT token generation

--- LoginService/src/main/java/com/test/taxiservice/loginservice/controllers/LoginServiceController.java
+++ LoginService/src/main/java/com/test/taxiservice/loginservice/controllers/LoginServiceController.java
@@ -2,14 +2,18 @@ package com.test.taxiservice.loginservice.controllers;
 
 import com.test.taxiservice.loginservice.constants.Constants;
 import com.test.taxiservice.loginservice.model.redis.AccessToken;
-import com.test.taxiservice.loginservice.redis.AccessTokenRepository;
+import com.test.taxiservice.loginservice.repository.redis.AccessTokenRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.Random;
 
 @RestController
@@ -19,6 +23,10 @@ public class LoginServiceController
   private AccessTokenRepository accessTokenRepository;
 
   private static final String BEARER_ = "Bearer ";
+  private static final long EXPIRY_DURATION = 24 * 60 * 60 * 1000; // 24 hour
+
+  @Value("${app.jwt.secret}")
+  private String SECRET_KEY;
 
   @GetMapping(Constants.LOGIN_PROFILE_URL)
   public String getProfile(HttpServletRequest request, HttpServletResponse response){
@@ -32,10 +40,18 @@ public class LoginServiceController
       @RequestParam(value = "mobileNumber", required = true) String mobileNumber){
 
     Random random = new Random();
-    int token = random.nextInt(999999);
+
+    String token = Jwts.builder()
+        .setSubject(String.format("%s",mobileNumber))
+        .setIssuer("TaxiService")
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRY_DURATION))
+        .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+        .compact();
 
     AccessToken accessToken = new AccessToken(
-        mobileNumber, BEARER_ + token, -1L);
+        mobileNumber, BEARER_ + token, EXPIRY_DURATION);
+
     accessTokenRepository.save(accessToken);
     return String.valueOf(token);
   }
